036c34f17d9cbc2c067306b762648a14ed1372cf
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2022-07-05 16:19:02 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabets in lower case
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{_putchar(j);
+		_putchar('\n');
+	}
+	return (0);
+}
