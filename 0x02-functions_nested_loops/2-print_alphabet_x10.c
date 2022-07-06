5179a8e814a7bdcbd96584e7b6ab31d7b8723b57
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2022-07-05 17:05:27 -0700

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabetx10 - prints 10 times the alphabet
+ * print_alphabet_x10 - prints 10 times the alphabet
  * in lowercase followed by a new line
  * Retrun: Always 0 (Success)
  */
-void print_alphabetx10(void)
+void print_alphabet_x10(void)
 {
 	int j;
 	int x;
