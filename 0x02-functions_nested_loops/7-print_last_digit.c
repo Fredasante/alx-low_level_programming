b43a91e6129a61c9ca40abffdd3ab4a2af0b9814
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2022-07-06 08:32:14 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,11 @@
  */
 int print_last_digit(int n)
 {
-	int last digit = n % 10;
+	int x;
 
-	if (last_digit < 0)
-		last_digit *= -1;
-	_putchar(last_digit + '0');
-
-	
-	return (last_digit);
+	if (n < 0)
+		n = -n;
+	x = n % 10;
+	_putchar(x + '0');
+	return (x);
 }
