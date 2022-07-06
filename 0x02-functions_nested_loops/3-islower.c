a7774bf40472815c512a5a5d16652f4270e6c66d
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2022-07-06 05:05:34 -0700

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - a function that checks for lowercase character
- * @c - a character argument
+ * @c: a character argument
  * Return: returns 1 and 0 depending on condition
  */
 int _islower(int c)
