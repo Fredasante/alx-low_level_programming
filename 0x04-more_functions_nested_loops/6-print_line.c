2075701f04ef9083e3c910239693e56924821393
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2022-07-07 10:45:29 -0700

    fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,11 +7,12 @@
 
 void print_line(int n)
 {
-	int x;
-
-	if (n <= 0)
+	if (n < 1)
 		_putchar('\n');
-	for (x = 0; x <= n; x++)
-		_putchar('_');
-	_putchar('\n');
+	else
+	{
+		while (n--)
+			_putchar('_');
+		_putchar('\n');
+	}
 }
