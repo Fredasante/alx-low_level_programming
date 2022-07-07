a206422a60f14060691dffd2e41ff1dbf583b1c5
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2022-07-07 08:48:40 -0700

    task 3 fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,9 @@
 
 void print_numbers(void)
 {
-	int ii = 0;
+	int x;
 
-	do {
-		_putchar(ii + '0');
-		ii++;
-	} while (ii < 10);
-
-	_putchar('\n');
+	for (x = 0; x < 10 ; x++)
+		_putchar(x + '0');
+			_putchar('\n');
 }
