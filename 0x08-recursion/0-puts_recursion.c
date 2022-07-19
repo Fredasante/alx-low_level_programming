ed37ceeb2c65538a2bdf9ef42f5c2e95f03c3779
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2022-07-19 11:42:30 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer to string to print
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+if (*s == '\0')
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+}
